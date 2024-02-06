6d577d5e359770ee5d608b498f1f7ec72d31af30
Gareth <gareth.w.owen@gmail.com>
2024-02-06 19:17:30 +0000

    Initial commit

---
+++ test/localstack-test-1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LocalstackTest1 from '../lib/localstack-test-1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/localstack-test-1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LocalstackTest1.LocalstackTest1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
