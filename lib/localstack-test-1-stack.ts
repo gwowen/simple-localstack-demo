caed657564182720f1888cc0ddabe6de5bce651b
Gareth <gareth.w.owen@gmail.com>
2024-02-06 20:30:09 +0000

    Initial commit

--- lib/localstack-test-1-stack.ts
+++ lib/localstack-test-1-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class LocalstackTest1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const stackTestQueue = new sqs.Queue(this, 'LocalstackTest1Queue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LocalstackTest1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const stackTestBucket = new s3.Bucket(this, 'stackTestBucket');
+
   }
 }
